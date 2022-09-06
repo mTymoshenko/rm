7c1fe9961f8942dc16d8b4a05771a06964e949d8
mTymoshenko <maksym.tymoshenko@litslink.com>
2022-09-06 14:18:41 +0300

    refactor api layer

---
+++ api/apollo.ts
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import { provideApolloClient } from "@vue/apollo-composable";
+
+const apolloClient = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+const apolloProvider = provideApolloClient(apolloClient);
+
+export default apolloProvider;
